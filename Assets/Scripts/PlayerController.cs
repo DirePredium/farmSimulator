2da4c5d72b11cccc071bd453a2ed8f6cb0e7453c
DirePredium <user83054@gmail.com>
2023-04-11 09:55:30 +0300

    Project finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalSpeed;
+    public float speed = 10f;
+    public float xRange = 10f;
+
+    public GameObject projectilPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalSpeed = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilPrefab, transform.position, projectilPrefab.transform.rotation);
+        }
     }
 }
